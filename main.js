00a2c6f638f1601a2d5a279945de52faf704e704
Siddarth-2011 <81513284+Siddarth-2011@users.noreply.github.com>
2022-04-09 09:52:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+img = "";
+objects = [];
+
+function preload(){
+    
+}
+
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+ if(status !="")
+{
+    for(i = 0; i< objects.length; i++)
+    {
+        document.getElementById("status").innerHTML = "Status : There are 6 big objects form which cocossd model has detected " +objects.length+ "obejcts";
+
+        fill("#FF0000");
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + "" + percent + "%", objects[i].x, objects[i].y);
+        noFill();
+        stroke("#FF0000");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+}
+    
+}
+
+
+
+
+
+function modelLoaded()
+{
+  console.log("Model Loaded!")
+  status = true;
+  objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+}
